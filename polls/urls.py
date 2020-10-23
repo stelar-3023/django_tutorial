14ec73a1ffa3bdafef7928a96df22a8b160e4a74
Steve Larsen <slarsen-3@att.net>
2020-10-23 17:27:27 -0500

    Amend URLconf

--- polls/urls.py
+++ polls/urls.py
@@ -6,9 +6,9 @@ app_name = 'polls'
 urlpatterns = [
     path("", views.index, name="index"),
     
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.detail, name="detail"),
     
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/results/", views.results, name="results"),
     
-    path("<int:question_id>/vote/", views.vote, name="vote")
+    path("<int:pk>/vote/", views.vote, name="vote")
     ]
